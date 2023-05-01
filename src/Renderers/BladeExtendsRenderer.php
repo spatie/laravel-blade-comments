92c11c687e6d4315697e8242ff81af4a6809fac5
freekmurze <freekmurze@users.noreply.github.com>
2023-05-01 09:04:17 +0000

    Fix styling

--- src/Renderers/BladeExtendsRenderer.php
+++ src/Renderers/BladeExtendsRenderer.php
@@ -24,7 +24,7 @@ class BladeExtendsRenderer implements Renderer
 
     protected function render(string $compiledViewContent, string $templatePath): string
     {
-        $startComment = "<!-- Start extends: {$templatePath} -->" . PHP_EOL;
+        $startComment = "<!-- Start extends: {$templatePath} -->".PHP_EOL;
 
         return "{$startComment}{$compiledViewContent}";
     }
--- tests/Renderers/BladeExtendsRendererTest.php
+++ tests/Renderers/BladeExtendsRendererTest.php
@@ -2,7 +2,7 @@
 
 use function Spatie\Snapshots\assertMatchesHtmlSnapshot;
 
-it('will add paths for extends', function() {
+it('will add paths for extends', function () {
     $renderedView = view('extends.page')->render();
 
     assertMatchesHtmlSnapshot($renderedView);
