292ce8f84b563a7e47e0c413732ff1ed03085704
Freek Van der Herten <freek@spatie.be>
2023-05-01 09:27:55 +0200

    Refactor BladePathsServiceProvider to use method chaining

--- src/BladePathsServiceProvider.php
+++ src/BladePathsServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Blade;
 use Illuminate\View\Compilers\BladeCompiler;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Livewire\Livewire;
 
 class BladePathsServiceProvider extends PackageServiceProvider
 {
@@ -18,12 +19,13 @@ class BladePathsServiceProvider extends PackageServiceProvider
 
     public function packageBooted()
     {
-        $this->addCommentsForIncludes();
-        $this->addCommentsToLayouts();
-        $this->addCommentsToLivewireTemplates();
+        $this
+            ->addCommentsForIncludes()
+            ->addCommentsToLayouts()
+            ->addCommentsToLivewireTemplates();
     }
 
-    protected function addCommentsForIncludes(): void
+    protected function addCommentsForIncludes(): self
     {
         Blade::directive('include', function ($expression) {
             $bladeCompiler = app(BladeCompiler::class);
@@ -34,11 +36,13 @@ class BladePathsServiceProvider extends PackageServiceProvider
             $startComment = "<!-- Start of partial: {$partial} -->\n";
             $endComment = "\n<!-- End of partial: {$partial} -->";
 
-            return $startComment.$compiled.$endComment;
+            return $startComment . $compiled . $endComment;
         });
+
+        return $this;
     }
 
-    protected function addCommentsToLayouts(): void
+    protected function addCommentsToLayouts(): self
     {
         Blade::directive('extends', function ($expression) {
             $bladeCompiler = app(BladeCompiler::class);
@@ -48,22 +52,24 @@ class BladePathsServiceProvider extends PackageServiceProvider
             $template = trim($expression, "'");
             $startComment = "<!-- Extended Layout: {$template} -->\n";
 
-            return $startComment.$compiled;
+            return $startComment . $compiled;
         });
+
+        return $this;
     }
 
-    protected function addCommentsToLivewireTemplates(): void
+    protected function addCommentsToLivewireTemplates(): self
     {
-        if (! class_exists(Livewire\Livewire::class)) {
+        if (!class_exists(Livewire::class)) {
             return;
         }
 
-        Livewire\Livewire::listen('component.dehydrate.initial', function ($component, $response) {
-            if (! $html = data_get($response, 'effects.html')) {
+        Livewire::listen('component.dehydrate.initial', function ($component, $response) {
+            if (!$html = data_get($response, 'effects.html')) {
                 return;
             }
 
-            $componentName = get_class($component).' ('.$component->getName().')';
+            $componentName = get_class($component) . ' (' . $component->getName() . ')';
 
             $startComment = "<!-- Start Livewire Component {$componentName} -->";
             $pos = strpos($html, '>') + 1;
@@ -71,7 +77,9 @@ class BladePathsServiceProvider extends PackageServiceProvider
 
             $endComment = "<!-- End of Livewire Component {$componentName} -->";
 
-            data_set($response, 'effects.html', $newHtml.$endComment);
+            data_set($response, 'effects.html', $newHtml . $endComment);
         });
+
+        return $this;
     }
 }
