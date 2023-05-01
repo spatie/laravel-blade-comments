e63340f9890e4075e32610cb533472ec7cd6ad8a
Freek Van der Herten <freek@spatie.be>
2023-05-01 11:05:56 +0200

    Register blade renderers in BladePathsServiceProvider

--- src/BladePathsServiceProvider.php
+++ src/BladePathsServiceProvider.php
@@ -22,13 +22,18 @@ class BladePathsServiceProvider extends PackageServiceProvider
             return;
         }
 
+        $this->registerRenderers();
+    }
+
+    protected function registerRenderers(): void
+    {
         collect(config('blade-paths.renderers'))
-            ->map(fn (string $rendererClass) => app($rendererClass))
+            ->map(fn(string $rendererClass) => app($rendererClass))
             ->each(function (object $renderer) {
-                if (! $renderer instanceof Renderer) {
+                if (!$renderer instanceof Renderer) {
                     throw InvalidRenderer::make($renderer);
                 }
             })
-            ->each(fn (Renderer $renderer) => $renderer->register());
+            ->each(fn(Renderer $renderer) => $renderer->register());
     }
 }
