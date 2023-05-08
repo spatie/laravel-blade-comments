1e56197822292f659aad1c310ec571b19d3d05fa
Tim Van Dijck <tim.van.dijck@gmail.com>
2023-05-08 11:55:27 +0200

    Add comments for Blade Components

--- src/Renderers/BladeComponentRenderer.php
+++ src/Renderers/BladeComponentRenderer.php
@@ -2,10 +2,35 @@
 
 namespace Spatie\BladePaths\Renderers;
 
+use Illuminate\Support\Facades\Blade;
+use Illuminate\View\Compilers\BladeCompiler;
+use Illuminate\View\Engines\CompilerEngine;
+use Spatie\BladePaths\CustomBladeCompiler;
+
 class BladeComponentRenderer implements Renderer
 {
+    public function __construct(protected BladeCompiler $compiler)
+    {
+    }
+
     public function register(): void
     {
-        // TODO: Implement register() method.
+        Blade::directive('component', function ($expression) {
+            $compiledViewContent = invade($this->compiler)->compileComponent($expression);
+
+            return $this->render($compiledViewContent, $expression);
+        });
+    }
+
+    protected function render(string $compiledViewContent, $expression): string
+    {
+        $expression = str_replace('\'', '', $expression);
+        $expressionParts = explode(', ', $expression);
+        $componentName = "{$expressionParts[0]} ({$expressionParts[1]})";
+
+        $startComment = "<!-- Start of component: {$componentName} -->".PHP_EOL;
+        $endComment = PHP_EOL."<!-- End of component: {$componentName} -->";
+
+        return "{$startComment}{$compiledViewContent}{$endComment}";
     }
 }
