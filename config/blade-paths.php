eb5133aa6ed4d8b0498a680c8f68c95edc330f17
Freek Van der Herten <freek@spatie.be>
2023-05-29 00:06:01 +0200

    Add comments and debug info to Blade views

--- config/blade-paths.php
+++ config/blade-paths.php
@@ -3,6 +3,10 @@
 return [
     'enable' => env('APP_ENV') !== 'production',
 
+    /*
+     * These classes provide regex for adding comments for
+     * various Blade directives.
+     */
     'commenters' => [
         Spatie\BladePaths\Precompilers\BladeCommenters\BladeStartComponentCommenter::class,
         Spatie\BladePaths\Precompilers\BladeCommenters\BlandeEndComponentCommenter::class,
@@ -15,10 +19,18 @@ return [
         Spatie\BladePaths\Precompilers\BladeCommenters\SectionCommenter::class,
     ],
 
+    /*
+     * This middleware will add extra debug information to the start
+     * of a rendered HTML page
+     */
     'middleware' => [
         Spatie\BladePaths\Middleware\AddDebugInfo::class,
     ],
 
+    /*
+     * This class is responsible for calling the registered Blade commenters.
+     * In most case, you don't need to modify this class.
+     */
     'precompilers' => [
         Spatie\BladePaths\Precompilers\BladeCommentsPrecompiler::class,
     ],
