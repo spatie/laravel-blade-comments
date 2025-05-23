6e23fcc7c09d2010a5df2c6b6c8c9fd9e43f4f50
Wouter van Dongen <wouter.vandongen@dongit.nl>
2025-05-23 20:00:13 +0200

    Rewrite ComponentCommenter

--- src/Commenters/BladeCommenters/BladeComponentCommenter.php
+++ src/Commenters/BladeCommenters/BladeComponentCommenter.php
@@ -20,27 +20,26 @@ class BladeComponentCommenter
             return $bladeContent;
         }
 
-        $componentCollections = $document->getComponents();
+        $document
+            ->getComponents()
+            ->transform(function (ComponentNode $node) {
+                $node->content = $this->addComments($node);
+                return $node;
+            });
 
-        foreach ($componentCollections as &$node) {
-            /** @var ComponentNode $node */
-            $node->content = $this->addComments($node);
-        }
         return $document->toString();
     }
 
-
-    // todo find path?
     private function htmlComment(ComponentNode $node, string $part = 'start'): string
     {
         $directive = 'component';
-//        $path = '';
         $name = $node->getName();
+        $path = app('blade.compiler')->getClassComponentAliases()[$name] ?? '';
 
         return strtr(($part === 'start' ? $this->startComment : $this->endComment), [
             ':directive' => $directive,
-//            ':path' => $path,
-            ':name' => $name,
+            ':path' => str($path)->wrap("'"),
+            ':name' => str($name)->wrap("'"),
         ]);
     }
 
@@ -50,104 +49,3 @@ class BladeComponentCommenter
     }
 
 }
-
-
-
-//<?php
-//
-//namespace Spatie\BladeComments\Commenters\BladeCommenters;
-//
-//use Stillat\BladeParser\Document\Document;
-//use Stillat\BladeParser\Nodes\Components\ComponentNode;
-//use Stillat\BladeParser\Nodes\DirectiveNode;
-//
-//class BladeComponentCommenter
-//{
-//
-//    protected string $startComment = '<!-- Start :directive :path :name -->';
-//
-//    protected string $endComment = '<!-- End :directive :path :name -->';
-//
-//    public function parse($bladeContent): string
-//    {
-//        $document = Document::fromText($bladeContent);
-//
-//        $componentCollections = $document->getComponents();
-//
-//        foreach ($componentCollections as &$node) {
-//
-//            /** @var ComponentNode $node */
-//dump($this->addComments($node));
-//            $node->sourceContent = $this->addComments($node);
-//
-//        }
-//        return $document->toString();
-//    }
-//
-//
-////    ##############################################################
-//
-////    public function parse($bladeContent): string
-////    {
-////        $document = Document::fromText($bladeContent);
-////
-////        $componentCollections = $document->findDirectivesByName('component');
-////
-////        foreach ($componentCollections as &$node) {
-////
-////            /** @var DirectiveNode $node */
-////
-////            $node->sourceContent = $this->addComments($node);
-////
-////        }
-////        return $document->toString();
-////    }
-////
-//    private function htmlComment(ComponentNode $node, string $part = 'start'): string
-//    {
-//        $directive = 'component';
-//        $path = 'pad';
-//        $name = $node->getName();
-//
-//        return strtr(($part === 'start' ? $this->startComment : $this->endComment), [
-//            ':directive' => $directive,
-//            ':path' => $path,
-//            ':name' => $name,
-//        ]);
-//    }
-////
-//    public function addComments(ComponentNode $node): string
-//    {
-//        return $this->htmlComment($node, 'start') . $node->toString() . $this->htmlComment($node, 'end');
-//    }
-////
-////
-////    /**
-////     * For Example: 'Spatie\BladeComments\Tests\TestSupport\BladeComponents\TestBladeComponent'
-////     */
-////    protected function getNodePath(DirectiveNode $node): string
-////    {
-////        return $node->arguments->getArgValues()->get(0);
-////    }
-////
-////
-////    /**
-////     * For Example: 'test-component'
-////     */
-////    protected function getNodeName(DirectiveNode $node): string
-////    {
-////        return $node->arguments->getArgValues()->get(1);
-////    }
-//
-//
-//
-////    public function pattern(): string
-////    {
-////        return '/##BEGIN-COMPONENT-CLASS##@component\(\\\'([^\']+)\\\', *\\\'([^\']+)\\\', *(\[[^\]]*\])\)(.*?)@endComponentClass##END-COMPONENT-CLASS##/s';
-////    }
-////
-////    public function replacement(): string
-////    {
-////        return '<!-- Start component \'$1\' \'$2\' -->$0<!-- End component \'$1\' \'$2\' -->';
-////    }
-//}
